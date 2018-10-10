0c69e6f666458f134c38ef295093252287f13f91
Kaljuk <erik.kaljumae@gmail.com>
2018-10-10 23:42:35 +0300

    Sonic shortened

--- W4/sonicTheHedgehog.py
+++ W4/sonicTheHedgehog.py
@@ -1,3 +1,2 @@
-def sonic_how_many_rings_did_you_get(input_string):
-    sumThis = [(1 if (c in "qQeRoOpPadDgb069") else 2 if(c in 'B8') else 0) for c in input_string]
-    return sum(sumThis)
+def sonic_how_many_rings_did_you_get(i):
+ return sum([1 if (c in "qQeRoOpPadDgb069") else 2 if(c in 'B8') else 0 for c in i])
---
+++ W4/sonicTheHedgehog_long.py
@@ -0,0 +1,3 @@
+def sonic_how_many_rings_did_you_get(input_string):
+    sumThis = [(1 if (c in "qQeRoOpPadDgb069") else 2 if(c in 'B8') else 0) for c in input_string]
+    return sum(sumThis) 
