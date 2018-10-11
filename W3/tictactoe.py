39e5af1159ec1f29877aba4e3648cc5cd838df42
Kaljuk <erik.kaljumae@gmail.com>
2018-10-11 06:48:39 +0300

    Doesn't get through the test for some reason

--- W3/tictactoe.py
+++ W3/tictactoe.py
@@ -1,49 +1,64 @@
 class TicTacToe():
-
     def __init__(self):
         self.field = [
-            ['', '', ''], # Row 0
-            ['', '', ''], # Row 1
-            ['', '', '']  # Row 2
+            ['', '', ''],  #  Row 0
+            ['', '', ''],  #  Row 1
+            ['', '', '']   #  Row 2
         ]
+        self.state = 3
+    # Check the game state, Game is over? Who won? Tie? Game is still unsfinished
+    def checkState(self):
+        field = self.field
+        state = 3
+        # Check for both players
+        for p in ['O', 'x']:
+            win = 0
+            # - Horizontal -
+            for row in field:
+                win = sum([1*(r == p) for r in row]) == 3
+            # | Vertical |
+            for col in range(3):
+                columns = [ field[0][col], field[1][col], field[2][col] ]
+                win = sum([1*(c == p) for c in columns]) == 3 or win
+            # \ Diagonal /
+            diagP = [ field[0][0], field[1][1], field[2][2] ]
+            diagS = [ field[0][2], field[1][1], field[2][0] ]
+            win = sum([1*(c == p) for c in diagP]) == 3 or win
+            win = sum([1*(c == p) for c in diagS]) == 3 or win
+            if win:
+                self.state = 1*(p == 'x')
+                return self.state
+        # Check if tie
+        tie = (sum([sum(r != '' for r in row) for row in field]) == 9)*2
+        self.state = tie if tie else state
 
     def add_move(self, row, col, player):
-        self.field[row][col] = player
+        self.field[row][col] = player if (self.field[row][col] == '') else self.field[row][col]
+        self.checkState()
 
     # 0 - Player with O's won
     # 1 - Player with X's won
     # 2 - It is a tie 
     # 3 - Game is still unfinished
     def get_state(self):
-        state = 3
-        # Rows
-        for row in self.field:
-            state = 0 if (set(row) == {'O'}) else 1 if (set(row) == {'x'}) else state
-        # Columns
-        for col in range(len(self.field)):
-            state = 0 if (self.field[0][col] == 'O' and self.field[1][col] == 'O' and self.field[2][col] == 'O') else state
-            state = 1 if (self.field[0][col] == 'x' and self.field[1][col] == 'x' and self.field[2][col] == 'x') else state
-        # Diagonals O
-        state = 0 if (self.field[0][0] == 'O' and self.field[1][1] == 'O' and self.field[2][2] == 'O') else state
-        state = 0 if (self.field[2][0] == 'O' and self.field[1][1] == 'O' and self.field[0][2] == 'O') else state
-        # Diagonals x
-        state = 1 if (self.field[0][0] == 'x' and self.field[1][1] == 'x' and self.field[2][2] == 'x') else state
-        state = 1 if (self.field[2][0] == 'x' and self.field[1][1] == 'x' and self.field[0][2] == 'x') else state
-        
-        # Check if TIE
-        if (state == 3):
-            if (('' not in self.field[0]) and ('' not in self.field[1]) and ('' not in self.field[2])):
-                state = 2
-        return state
+        return self.state
+
+    def showField(self):
+        print("Field:", self.field)
 
 # t = TicTacToe()
-# t.add_move(0,0,'O')
-# t.add_move(0,1,'x')
-# t.add_move(0,2,'O')
-# t.add_move(1,0,'x')
-# t.add_move(1,1,'O')
-# t.add_move(1,2,'x')
-# t.add_move(2,0,'x')
-# t.add_move(2,1,'O')
+# t.add_move(0,0,'x')
+# t.add_move(0,1,'O')
+# t.add_move(0,2,'x')
+
+# t.add_move(1,0,'O')
+# t.add_move(1,1,'x')
+# t.add_move(1,2,'O')
+
+# t.add_move(2,0,'O')
+# t.add_move(2,1,'x')
+# t.add_move(2,2,'O')
 # t.add_move(2,2,'x')
+
+# t.showField()
 # print(t.get_state())
